646cde2b9b676c521bd9849fff719bbb6f4d1acd
ivano.scifoni <iscifoni@libero.it>
2017-07-06 19:05:19 +0200

    Add project SharoBatch.Tracking.DB

---
+++ src/SharpBatch.Tracking.DB/DependencyInjection/TrackingDbServiceCollectionExtention.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpBatch.Tracking.DB.DependencyInjection
+{
+    public static class TrackingDbServiceCollectionExtention
+    {
+    }
+}
---
+++ src/SharpBatch.Tracking.DB/TrackingDb.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SharpBatch.Tracking.Abstraction;
+
+namespace SharpBatch.Tracking.DB
+{
+    public class TrackingDb : ISharpBatchTracking
+    {
+        public List<BatchTrackingModel> GetDataOfBatchName(string batchName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BatchTrackingModel> GetErrors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetErrorsCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BatchTrackingModel> GetRunning()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetRunningCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BatchTrackingModel> GetStatusAsync(Guid SessionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PingAsync(Guid sessionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StartAsync(string BatchName, Guid sessionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(Guid sessionId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
