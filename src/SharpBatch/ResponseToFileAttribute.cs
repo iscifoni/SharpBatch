1bf9f2f3eaafe3460c2930e1ef61f5aa5d9ae1bc
ivano.scifoni <iscifoni@libero.it>
2017-08-31 15:43:04 +0200

    Fix #10
    code Bit  clean
    Test class for for attribute ResponseToFileAttribute
    Path type to string

--- src/SharpBatch/ResponseToFileAttribute.cs
+++ src/SharpBatch/ResponseToFileAttribute.cs
@@ -1,4 +1,18 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿//Copyright 2016 Scifoni Ivano
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,57 +22,90 @@ using System.Threading.Tasks;
 
 namespace SharpBatch
 {
+    /// <summary>
+    /// Attribute used to send Batch response to file.
+    /// </summary>
     public class ResponseToFileAttribute : BatchExecutionAttribute
     {
+        /// <summary>
+        /// Filename to create
+        /// </summary>
         public string FileName { get; set; } = "Export";
-        public string FileExention { get; set; } = "exp";
-        public PathString Path { get; set; } = "";
 
+        /// <summary>
+        /// Extention file
+        /// </summary>
+        public string FileExention { get; set; } = "exp";
+
+        /// <summary>
+        /// File path
+        /// </summary>
+        public string Path { get; set; } = "";
+
+        /// <summary>
+        /// If true add a time stamp to filename
+        /// </summary>
         public bool TimeStampTocken { get; set; } = true;
 
+        /// <summary>
+        /// If true add SessionId to filename
+        /// </summary>
         public bool SessionIdInFileName { get; set; } = true;
 
         public override int Order { get; set; } = 10000;
+
+        /// <summary>
+        /// Return the filename generated 
+        /// </summary>
+        public string FullFileName { get; private set; }
 
         public override void onExecuted(BatchExecutionContext context)
         {
             var encoder = new UTF8Encoding();
             char[] content;
             var response = context.ShareMessage.Get<IResponseObject>();
+            var responseType = response.Response.GetType();
 
-            if ( typeof(byte[]).GetTypeInfo().IsAssignableFrom(response.Response.GetType()))
+            if (typeof(byte[]).GetTypeInfo().IsAssignableFrom(responseType))
             {
-                content = encoder.GetChars( (byte[])response.Response);
-            }
-            else if(typeof(string).GetTypeInfo().IsAssignableFrom(response.Response.GetType()))
-            {
-                content = ((string)response.Response).ToCharArray();
-            }else if (typeof(char[]).GetTypeInfo().IsAssignableFrom(response.Response.GetType()))
-            {
-                content = (char[])response.Response;
+                content = encoder.GetChars((byte[])response.Response);
             }
             else
             {
-                throw new NotSupportedException("Response type not supported for ResponseToFileAttribute");
+                if (typeof(string).GetTypeInfo().IsAssignableFrom(responseType))
+                {
+                    content = ((string)response.Response).ToCharArray();
+                }
+                else
+                {
+                    if (typeof(char[]).GetTypeInfo().IsAssignableFrom(responseType))
+                    {
+                        content = (char[])response.Response;
+                    }
+                    else
+                    {
+                        throw new NotSupportedException("Response type not supported for ResponseToFileAttribute");
+                    }
+                }
             }
 
-            string fullFileName = FileName;
+            FullFileName = $"{Path.ToString()}{FileName}";
             if (SessionIdInFileName)
             {
-                fullFileName += $"-{response.SessionId.ToString()}";
+                FullFileName += $"-{response.SessionId.ToString()}";
             }
 
             if (TimeStampTocken)
             {
-                fullFileName += $"-{DateTime.Now.ToString("yyyyMMddHHmmssfff")}";
+                FullFileName += $"-{DateTime.Now.ToString("yyyyMMddHHmmssfff")}";
             }
 
             if (!string.IsNullOrEmpty(FileExention))
             {
-                fullFileName += FileExention;
+                FullFileName += $".{FileExention}";
             }
 
-            var logFile = System.IO.File.Create(fullFileName);
+            var logFile = System.IO.File.Create(FullFileName);
             using(var logWriter = new System.IO.StreamWriter(logFile))
             {
                 var logTask = logWriter.WriteAsync(content);
---
+++ test/SharpBatchTest/ResponseToFileAttributeResponseTest.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using SharpBatch;
+using SharpBatch.internals;
+using Xunit;
+
+namespace SharpBatchTest
+{
+    public class ResponseToFileAttributeResponseTest
+    {
+        [Theory]
+        [InlineData("txt", "FILENAME",false, false, "", "FILENAME.txt", "ce8a738b-2534-4060-9f1c-9da6eba4aef8")]
+        [InlineData("txt", "FILENAME", true, false, "", "FILENAME-ce8a738b-2534-4060-9f1c-9da6eba4aef8.txt", "ce8a738b-2534-4060-9f1c-9da6eba4aef8")]
+        public void ResponseToFileAttributeResponse_FullFileName(string fileExtention, string fileName, 
+                                                                bool sessionIdInFileName, bool timeStampToken, 
+                                                                string path, string expected, string sessionId)
+        {
+            //Arrange
+            var attribute = createAttribute(fileExtention, fileName, sessionIdInFileName, timeStampToken, path);
+            var context = createBatchExecutionContext(new Guid(sessionId), "TEST TEST TEST");
+            
+            //Act
+            attribute.onExecuted(context);
+
+            //Assert
+            Assert.Equal(expected, attribute.FullFileName);
+
+        }
+
+        [Fact]
+        public void ResponseToFileAttributeResponse_onExecuted_StringContent()
+        {
+            //Arrange
+            var attribute = createAttribute("txt", "FILENAME", false, false, "");
+            var context = createBatchExecutionContext(Guid.NewGuid(), "TEST TEST TEST");
+
+            //Act
+            var ex = Record.Exception(() => attribute.onExecuted(context));
+
+            //Assert
+            Assert.Null(ex);
+        }
+
+        [Fact]
+        public void ResponseToFileAttributeResponse_onExecuted_ByteArrayContent()
+        {
+            //Arrange
+            var attribute = createAttribute("txt", "FILENAME", false, false, "");
+            var context = createBatchExecutionContext(Guid.NewGuid(), new byte[] { 123, 221, 213, 128, 165} );
+
+            //Act
+            var ex = Record.Exception(() => attribute.onExecuted(context));
+
+            //Assert
+            Assert.Null(ex);
+        }
+
+        [Fact]
+        public void ResponseToFileAttributeResponse_onExecuted_NotSavedClassContent()
+        {
+            //Arrange
+            var attribute = createAttribute("txt", "FILENAME", false, false, "");
+            var context = createBatchExecutionContext(Guid.NewGuid(), new WrongType());
+
+            //Act
+            var ex = Record.Exception(() => attribute.onExecuted(context));
+
+            //Assert
+            Assert.NotNull(ex);
+        }
+
+        [Fact]
+        public void ResponseToFileAttributeResponse_onExecuted_PathSpecified()
+        {
+            //Arrange
+            var attribute = createAttribute("txt", "FILENAME", false, false, Path.GetTempPath());
+            var context = createBatchExecutionContext(Guid.NewGuid(), new byte[] { 123, 221, 213, 128, 165 });
+
+            //Act
+            var ex = Record.Exception(() => attribute.onExecuted(context));
+
+            //Assert
+            Assert.Null(ex);
+        }
+        private BatchExecutionContext createBatchExecutionContext(Guid sessionId, object content)
+        {
+            var shareMessage = new ShareMessageCollection();
+            var responseObject = new ResponseObject(content, sessionId);
+            shareMessage.Set<IResponseObject>(responseObject);
+
+            BatchExecutionContext context = new BatchExecutionContext()
+            {
+                SessionId =sessionId,
+                ShareMessage = shareMessage
+            };
+
+            return context;
+        }
+
+        private ResponseToFileAttribute createAttribute(string fileExtention, string fileName,
+                                                                bool sessionIdInFileName, bool timeStampToken,
+                                                                string path)
+        {
+            return new ResponseToFileAttribute()
+            {
+                FileExention = fileExtention,
+                FileName = fileName,
+                SessionIdInFileName = sessionIdInFileName,
+                TimeStampTocken = timeStampToken,
+                Path = path
+            };
+        }
+
+        private class WrongType
+        {
+            public string Property1 { get; }
+        }
+    }
+}
